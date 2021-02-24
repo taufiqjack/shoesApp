0cee942d8aac9c10705835575287590ca8297552
taufiqjack <taufiqdwic@gmail.com>
2021-02-24 23:36:10 +0700

    custom colors

---
+++ lib/custom/warna.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+class Warna {
+  static const strawberry = const Color(0xffec2e34);
+  static const celery = const Color(0xffd2f7a0);
+  static const celery_white = const Color(0xff1fd2f7a0);
+  static const brownish_grey = const Color(0xff707070);
+  static const warm_pink = const Color(0xfff84f6f);
+  static const warm_pink_white = const Color(0xff1ff84f6f);
+  static const black = const Color(0xff000000);
+  static const pale_blue = const Color(0xffdce6e9);
+  static const white = const Color(0xffffffff);
+  static const light_navy = const Color(0xff17579f);
+  static const light_navy_white = const Color(0xff1f17579f);
+  static const white_two = const Color(0xfffafafa);
+  static const purpley_grey = const Color(0xff858283);
+  static const cornflower_blue = const Color(0xff4d8fd9);
+  static const disable = const Color(0xffa7a8ac);
+  static const white_three = const Color(0xffebebeb);
+  static const blue_sea = const Color(0xff309ABB);
+  static const blue_navy = const Color(0xff8ED0E2);
+  static const light_yellow = const Color(0xfffff33);
+  static const green = const Color(0xf004e00);
+}
