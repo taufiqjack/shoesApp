8e039a923ef5f60bedf567b43e5280e2107a4d15
taufiqjack <taufiqdwic@gmail.com>
2021-02-25 10:05:25 +0700

    adding models

---
+++ lib/models/kategoriModel.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KategoriModel {
+  String images;
+  String title;
+
+  KategoriModel({this.images, this.title});
+}
+
+List<KategoriModel> kategoriModel = [
+  KategoriModel(images: "assets/icons/shoes.svg", title: "Sneakers"),
+  KategoriModel(images: "assets/icons/watch.svg", title: "Watch"),
+  KategoriModel(images: "assets/icons/backpack.svg", title: "Backpack"),
+];
